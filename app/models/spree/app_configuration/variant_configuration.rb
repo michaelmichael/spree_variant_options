29cc649581a7f54449ab563c5d752b46803e6a2a
Michael Caviness <mcaviness@tcwequipment.com>
2015-01-24 19:14:57 -0800

    Change defaults to show instock products

--- app/models/spree/app_configuration/variant_configuration.rb
+++ app/models/spree/app_configuration/variant_configuration.rb
@@ -1,7 +1,7 @@
 module SpreeVariantOptions
   class VariantConfiguration < Spree::Preferences::Configuration
-    preference :allow_select_outofstock, :boolean, :default => false
-    preference :default_instock, :boolean, :default => false
+    preference :allow_select_outofstock, :boolean, default: true
+    preference :default_instock, :boolean, default: true
     preference :option_value_url, :string, :default => '/spree/option_values/:id/:style/:basename.:extension'
     preference :option_value_path, :string, :default => ':rails_root/public/spree/option_values/:id/:style/:basename.:extension'
     preference :option_value_styles, :string, default: "{\"small\":\"40x30#\",\"large\":\"140x110#\"}"
